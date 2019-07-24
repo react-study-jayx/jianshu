039c927bc0fa3674ecd145213fb7cd0d979c4b85
YongjieXue <yongjiexue@YongjieXuedeMacBook-Pro.local>
2019-07-24 18:43:42 +0800

    fix-home

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './common/header'
+import {connect} from  'react-redux'
+import {changeSearchAction,initValueAsyncAction} from './store/storeActions'
+import {BrowserRouter,Route} from 'react-router-dom'
+import Home from './pages/home'
+import Detail from './pages/detail'
+import SideBar from './common/sideBar'
+class App extends React.Component{
+  constructor(props){
+    super(props);
+  }
+ 
+  componentWillMount(){
+    this.props.initSearchValue()
+  }     
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render(){
+    return (
+      <div className="App">
+        <Header></Header>
+        <BrowserRouter>
+          
+              {/* <Route path='/' exact render={()=><div>Index</div>}></Route>
+              <Route path='/detail' exact render={()=><div>Detail</div>}></Route> */}
+
+              <Route path='/' exact  component={Home}></Route>
+              <Route path='/detail' exact  component={Detail}></Route>
+            
+        </BrowserRouter>
+        <SideBar></SideBar>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps=(state)=>{
+      return {
+        searchValue:state.searchValue
+      }
+}
+const mapDispatchToProps=(dispatch)=>{
+  return {
+    handleSearchChange(e){
+      let value=e.target.value;
+      const action=changeSearchAction(value);
+      dispatch(action)
+    },
+    initSearchValue(){
+      let action=initValueAsyncAction();
+      dispatch(action);
+    }
+  }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(App)
+//export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//import './index.css';
+import   * as  Style from './style'
+import {IconFont} from './statics/iconfont/'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './store'
+import {Provider} from 'react-redux'
+ReactDOM.render(//全局样式的引入
+<React.Fragment>
+    <Provider store={store}>
+        <App />
+    </Provider>
+    <Style.GlobaleStyle ></Style.GlobaleStyle>
+    <IconFont></IconFont>
+</React.Fragment>,
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
