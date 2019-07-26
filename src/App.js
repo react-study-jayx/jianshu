292d7d0e51d7fc56f289fde2504c464bad6eb8f0
YongjieXue <yongjiexue@YongjieXuedeMacBook-Pro.local>
2019-07-26 10:28:33 +0800

    add

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {changeSearchAction,initValueAsyncAction} from './store/storeActions'
 import {BrowserRouter,Route} from 'react-router-dom'
 import Home from './pages/home'
 import Detail from './pages/detail'
+import Login from './pages/login'
 import SideBar from './common/sideBar'
 class App extends React.Component{
   constructor(props){
@@ -26,6 +27,7 @@ class App extends React.Component{
 
               <Route path='/' exact  component={Home}></Route>
               <Route path='/detail' exact  component={Detail}></Route>
+              <Route path='/login' exact  component={Login}></Route>
             
         </BrowserRouter>
         <SideBar></SideBar>
